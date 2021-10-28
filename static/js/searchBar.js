45c54d76e93c916534f0ba785f5f92d51fc703c0
Quy Thanh <59784983+quythanh@users.noreply.github.com>
2021-10-28 17:42:47 +0700

    rewrite code with jquery

--- static/js/searchBar.js
+++ static/js/searchBar.js
@@ -1,25 +1,23 @@
 // cSpell:words Tokisaki Kurumi
 
-txt = $("#searchbar-text");
-
 $("#searchbar").click((event) => {
     switch (event.target.id) {
         case "search-by-voice":
-            txt.textContent = "lmao, chả ai xài cái nút này cả";
+            $("#searchbar-text").text("lmao, chả ai xài cái nút này cả");
             break;
 
         case "search-button":
-            ($("#google-content").className == "col-7 hidden") ?
-            $("#google-content").className = "col-7":
-                txt.textContent = "kết quả đã hiện sẵn rồi mà =]";
+            ($("#google-content").hasClass("hidden")) ?
+            $("#google-content").removeClass("hidden"):
+                $("#searchbar-text").text("kết quả đã hiện sẵn rồi mà =]");
             break;
 
         default:
-            txt.textContent = "không được tìm cái khác đâu";
+            $("#searchbar-text").text("không được tìm cái khác đâu");
             break;
     }
 
     lastTimeout = setTimeout(() => {
-        txt.textContent = "Tokisaki Kurumi";
+        $("#searchbar-text").text("Tokisaki Kurumi");
     }, 2000);
 });
